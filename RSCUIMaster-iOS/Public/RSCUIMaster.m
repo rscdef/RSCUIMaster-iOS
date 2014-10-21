7597f79d549b6a44b98bb9ca85adc261e2c04a15
rscdef <rscdef@gmail.com>
2014-10-21 12:06:52 +0800

    change the license to MIT

---
+++ RSCUIMaster-iOS/Public/RSCUIMaster.m
@@ -0,0 +1,13 @@
+//
+//  RSCUIMaster.m
+//  RSCUIMaster-iOS
+//
+//  Created by Luke on 10/21/14.
+//  Copyright (c) 2014 UIMaster. All rights reserved.
+//
+
+#import "RSCUIMaster.h"
+
+@implementation RSCUIMaster
+
+@end
