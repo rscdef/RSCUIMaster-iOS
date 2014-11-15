e2e8973df3b070610b6feeeb35e85097d5f17fb4
rscdef <rscdef@gmail.com>
2014-11-15 20:42:11 +0800

    structure for app

---
+++ RSCUIMaster/RSCUIMaster/RSCPlaygroundController.m
@@ -0,0 +1,32 @@
+//
+//  RSCPlaygroundController.m
+//  RSCUIMaster
+//
+//  Created by rscdef@gmail.com on 14/11/15.
+//  Copyright (c) 2014年 UIMaster. All rights reserved.
+//
+
+#import "RSCPlaygroundController.h"
+
+@interface RSCPlaygroundController ()
+
+@end
+
+@implementation RSCPlaygroundController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Playground";
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
